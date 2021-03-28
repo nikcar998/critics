8b605692be478ab1fc720b2c35e6a0daf3e3ef1a
nikcar <nikcar34@gmail.com>
2021-03-28 18:21:58 +0200

    reviewForm with formik

--- critics-client-app/src/features/reviews/ReviewForm.tsx
+++ critics-client-app/src/features/reviews/ReviewForm.tsx
@@ -2,8 +2,8 @@ import React, { ChangeEvent, Fragment, useEffect, useState } from "react";
 import { useMediaQuery } from "react-responsive";
 import { Redirect } from "react-router";
 import {
+  Button,
   DropdownProps,
-  Form,
   Grid,
   Header,
   Image,
@@ -16,6 +16,13 @@ import axios from "axios";
 import ValidationErrors from "../errors/ValidationErrors";
 import { observer } from "mobx-react-lite";
 import { history } from "../..";
+import { ratingOptions } from "../../app/common/options/ratingOptions";
+import { Form, Formik } from "formik";
+import MyTextInput from "../../app/common/form/MyTextInput";
+import MyTextArea from "../../app/common/form/MyTextArea";
+import MySelectInput from "../../app/common/form/MySelectInput";
+import * as Yup from "yup";
+import { Review } from "../../app/models/review";
 
 //this component is necessary to store the reviews
 const ReviewForm = () => {
@@ -28,16 +35,6 @@ const ReviewForm = () => {
   const imageUrl =
     "https://image.tmdb.org/t/p/w500" + filmStore.selectedFilm?.poster_path;
   const defaultImageUrl = "/images/no_picture_available.jpg";
-
-  //values necessary for the <Select /> element, that stores the review's rating
-  const ratingOptions = [
-    { key: 0, value: 0, text: 0 },
-    { key: 1, value: 1, text: 1 },
-    { key: 2, value: 2, text: 2 },
-    { key: 3, value: 3, text: 3 },
-    { key: 4, value: 4, text: 4 },
-    { key: 5, value: 5, text: 5 },
-  ];
 
   //TODO -> bisongna rendere non statico lo user_id /////////////////////////////////////////////
   const initialState = {
@@ -58,10 +55,11 @@ const ReviewForm = () => {
   const [error, setError] = useState<string[]>([]);
 
   //TODO -> togliere la richiesta di crsf token
-  const handleSubmit = () => {
+  const handleFormSubmit = (review: Review) => {
+    console.log(review);
     axios.get("/sanctum/csrf-cookie").then((response) => {
       reviewStore.storeReview(review).then(() => {
-        if (reviewStore.errors) {
+        if (reviewStore.errors[0] !== null) {
           setError(reviewStore.errors);
         } else {
           history.push("/reviews");
@@ -70,21 +68,11 @@ const ReviewForm = () => {
     });
   };
 
-  const handleInputChange = (
-    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {
-    const { name, value } = event.currentTarget;
-    setReview({ ...review, [name]: value });
-  };
-
-  const handleSelectChange = (
-    event: React.SyntheticEvent<HTMLElement, Event>,
-    data: DropdownProps
-  ) => {
-    if (typeof data.value == "number") {
-      setReview({ ...review, rating: data.value });
-    }
-  };
+  const validationSchema = Yup.object({
+    title: Yup.string().required("The title is required"),
+    opinion: Yup.string().required("The opinion is required"),
+    rating: Yup.string().required("The rating is required"),
+  });
 
   //the necessary values will be added to the review-state, those are necessary to store the new review
   useEffect(() => {
@@ -121,40 +109,46 @@ const ReviewForm = () => {
               />
             </Grid.Column>
             <Grid.Column width={isDesktop ? 12 : 16}>
-              <Header as="h3" style={{ marginTop: 10 }}>
+              <Header as="h2" style={{ marginTop: 10 }}>
                 {filmStore.selectedFilm.title}
               </Header>
-
-              <Form onSubmit={handleSubmit} error>
-                {error[0] != null && <ValidationErrors errors={error} />}
-                <Form.Input
-                  name="title"
-                  required
-                  label="Title"
-                  value={review.title}
-                  placeholder="The title of your review"
-                  onChange={handleInputChange}
-                />
-                <Form.TextArea
-                  name="opinion"
-                  value={review.opinion}
-                  label="Opinion"
-                  placeholder="Write what you think about the movie"
-                  required
-                  onChange={handleInputChange}
-                  style={{ float: "right" }}
-                />
-                <Label size="large">Rating:</Label>
-                <Select
-                  style={{ marginTop: 10 }}
-                  placeholder="0"
-                  options={ratingOptions}
-                  onChange={handleSelectChange}
-                />
-                <Form.Button style={{ marginTop: 10 }} primary>
-                  Submit
-                </Form.Button>
-              </Form>
+              {error[0] != null && <ValidationErrors errors={error} />}
+              <Formik
+                validationSchema={validationSchema}
+                enableReinitialize
+                initialValues={review}
+                onSubmit={(values) => handleFormSubmit(values)}
+              >
+                {({ handleSubmit, isSubmitting, dirty, isValid }) => (
+                  <Form className="ui form" onSubmit={handleSubmit}>
+                    <MyTextInput
+                      name="title"
+                      label="Title"
+                      placeholder="The title of your review"
+                    />
+                    <MyTextArea
+                      name="opinion"
+                      label="Opinion"
+                      placeholder="Write what you think about the movie"
+                      rows={3}
+                    />
+                    <Label size="large">Rating:</Label>
+                    <MySelectInput
+                      name="rating"
+                      placeholder="0"
+                      options={ratingOptions}
+                    />
+                    <Button
+                      disabled={isSubmitting || !dirty || !isValid}
+                      type="submit"
+                      style={{ marginTop: 10 }}
+                      primary
+                    >
+                      Submit
+                    </Button>
+                  </Form>
+                )}
+              </Formik>
             </Grid.Column>
           </Grid.Row>
         </Grid>
